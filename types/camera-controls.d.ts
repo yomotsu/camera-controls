41075ac0a834bd825899cff587bddd961a387945
FMS-Cat <yutaka.obuchi@gmail.com>
2018-12-26 17:35:23 +0900

    Type Definitions: now this is usuable

--- types/camera-controls.d.ts
+++ types/camera-controls.d.ts
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
 import { EventDispatcher } from './event-dispatcher';
 
-export class CameraControls extends EventDispatcher {
+export { EventDispatcher, Event } from './event-dispatcher';
+
+export default class CameraControls extends EventDispatcher {
   // static methods
-  static install(libs: {THREE: THREE});
+  static install(libs: {THREE}): void;
 
   // constructor
   constructor(object: THREE.Object3D, domElement?: HTMLElement, options?: { ignoreDOMEventListeners?: boolean });
@@ -27,49 +29,49 @@ export class CameraControls extends EventDispatcher {
   public verticalDragToForward: boolean;
 
   // public methods
-  public rotate: (rotX: number, rotY: number, enableTransition?: boolean) => void;
-  public rotateTo: (rotX: number, rotY: number, enableTransition?: boolean) => void;
-  public dolly: (distance: number, enableTransition?: boolean) => void;
-  public dollyTo: (distance: number, enableTransition?: boolean) => void;
-  public pan: (x: number, y: number, enableTransition?: boolean) => void;
-  public truck: (x: number, y: number, enableTransition?: boolean) => void;
-  public forward: (distance: number, enableTransition?: boolean) => void;
-  public move: (x: number, y: number, z: number, enableTransition?: boolean) => void;
-  public fitTo: (objectOrBox3: THREE.Object3D | THREE.Box3, enableTransition?: boolean, options?: {
+  public rotate(rotX: number, rotY: number, enableTransition?: boolean): void;
+  public rotateTo(rotX: number, rotY: number, enableTransition?: boolean): void;
+  public dolly(distance: number, enableTransition?: boolean): void;
+  public dollyTo(distance: number, enableTransition?: boolean): void;
+  public pan(x: number, y: number, enableTransition?: boolean): void;
+  public truck(x: number, y: number, enableTransition?: boolean): void;
+  public forward(distance: number, enableTransition?: boolean): void;
+  public move(x: number, y: number, z: number, enableTransition?: boolean): void;
+  public fitTo(objectOrBox3: THREE.Object3D | THREE.Box3, enableTransition?: boolean, options?: {
     paddingLeft?: number,
     paddingRight?: number,
     paddingBottom?: number,
     paddingTop?: number
-  }) => void;
-  public setLookAt: (
+  }): void;
+  public setLookAt(
     positionX: number, positionY: number, positionZ: number,
     targetX: number, targetY: number, targetZ: number,
     enableTransition?: boolean
-  ) => void;
-  public lerpLookAt: (
+  ): void;
+  public lerpLookAt(
     positionAX: number, positionAY: number, positionAZ: number,
     targetAX: number, targetAY: number, targetAZ: number,
     positionBX: number, positionBY: number, positionBZ: number,
     targetBX: number, targetBY: number, targetBZ: number,
     x: number, enableTransition?: boolean
-  ) => void;
-  public setPosition: (positionX: number, positionY: number, positionZ: number, enableTransition?: boolean) => void;
-  public setTarget: (targetX: number, targetY: number, targetZ: number, enableTransition?: boolean) => void;
-  public getDistanceToFit: (width: number, height: number, depth: number) => number;
-  public getTarget: (out?: THREE.Vector3) => THREE.Vector3;
-  public getPosition: (out?: THREE.Vector3) => THREE.Vector3;
-  public reset: (enableTransition?: boolean) => void;
-  public saveState: () => void;
-  public update: (delta: number) => void;
-  public toJSON: () => string;
-  public fromJSON: (json: string, enableTransition?: boolean) => void;
-  public dispose: () => void;
+  ): void;
+  public setPosition(positionX: number, positionY: number, positionZ: number, enableTransition?: boolean): void;
+  public setTarget(targetX: number, targetY: number, targetZ: number, enableTransition?: boolean): void;
+  public getDistanceToFit(width: number, height: number, depth: number): number;
+  public getTarget(out?: THREE.Vector3): THREE.Vector3;
+  public getPosition(out?: THREE.Vector3): THREE.Vector3;
+  public reset(enableTransition?: boolean): void;
+  public saveState(): void;
+  public update(delta: number): void;
+  public toJSON(): string;
+  public fromJSON(json: string, enableTransition?: boolean): void;
+  public dispose(): void;
 
   // private members
   private _state: STATE;
 
   // private methods
-  private _sanitizeSphericals: () => void;
+  private _sanitizeSphericals(): void;
 }
 
 export enum STATE {
--- types/event-dispatcher.d.ts
+++ types/event-dispatcher.d.ts
@@ -9,8 +9,8 @@ export class EventDispatcher {
   private _listeners: { [type: string]: Array<(event: Event) => void> };
 
   // public methods
-  public addEventListener: (type: string, listener: (event: Event) => void) => void;
-  public hasEventListener: (type: string, listener: (event: Event) => void) => boolean;
-  public removeEventListener: (type: string, listener: (event: Event) => void) => void;
-  public dispatchEvent: (event: { type: string; [attachment: string]: any; }) => void;
+  public addEventListener(type: string, listener: (event: Event) => void): void;
+  public hasEventListener(type: string, listener: (event: Event) => void): boolean;
+  public removeEventListener(type: string, listener: (event: Event) => void): void;
+  public dispatchEvent(event: { type: string; [attachment: string]: any; }): void;
 }
