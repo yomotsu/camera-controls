0d43798e7fb950b4fdc491ef1e2ad7ba1bea619a
Lewy Blue <lewy@discoverthreejs.com>
2021-08-30 09:00:39 +0700

    remove dist/

--- dist/utils/extractClientCoordFromEvent.d.ts
+++
@@ -1,3 +0,0 @@
-import type * as _THREE from 'three';
-import type { PointerInput } from '../types';
-export declare function extractClientCoordFromEvent(pointers: PointerInput[], out: _THREE.Vector2): void;
--- dist/utils/isTouchEvent.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isTouchEvent(event: Event): boolean;
