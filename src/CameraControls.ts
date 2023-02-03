17922299d4083e3bd2489d23a34642e82227d513
Akihiro Oyamada <admin@yomotsu.net>
2023-02-04 00:06:31 +0900

    Stop movement on drag start (#357)
    * avoid divide-by-zero
    
    * stop movement on dragstart
    
    * kill velocity

--- src/CameraControls.ts
+++ src/CameraControls.ts
@@ -941,6 +941,71 @@ export class CameraControls extends EventDispatcher {
 
 			}
 
+			// stop current movement on drag start
+			if (
+				( this._state & ACTION.ROTATE ) === ACTION.ROTATE ||
+				( this._state & ACTION.TOUCH_ROTATE ) === ACTION.TOUCH_ROTATE ||
+				( this._state & ACTION.TOUCH_DOLLY_ROTATE ) === ACTION.TOUCH_DOLLY_ROTATE ||
+				( this._state & ACTION.TOUCH_ZOOM_ROTATE ) === ACTION.TOUCH_ZOOM_ROTATE
+			) {
+
+				this._sphericalEnd.theta = this._spherical.theta;
+				this._sphericalEnd.phi = this._spherical.phi;
+				this._thetaVelocity.value = 0;
+				this._phiVelocity.value = 0;
+
+			}
+
+			if (
+				( this._state & ACTION.TRUCK ) === ACTION.TRUCK ||
+				( this._state & ACTION.TOUCH_TRUCK ) === ACTION.TOUCH_TRUCK ||
+				( this._state & ACTION.TOUCH_DOLLY_TRUCK ) === ACTION.TOUCH_DOLLY_TRUCK ||
+				( this._state & ACTION.TOUCH_ZOOM_TRUCK ) === ACTION.TOUCH_ZOOM_TRUCK
+			) {
+
+				this._targetEnd.copy( this._target );
+				this._targetVelocity.set( 0, 0, 0 );
+
+			}
+
+			if (
+				( this._state & ACTION.DOLLY ) === ACTION.DOLLY ||
+				( this._state & ACTION.TOUCH_DOLLY ) === ACTION.TOUCH_DOLLY ||
+				( this._state & ACTION.TOUCH_DOLLY_TRUCK ) === ACTION.TOUCH_DOLLY_TRUCK ||
+				( this._state & ACTION.TOUCH_DOLLY_OFFSET ) === ACTION.TOUCH_DOLLY_OFFSET ||
+				( this._state & ACTION.TOUCH_DOLLY_ROTATE ) === ACTION.TOUCH_DOLLY_ROTATE
+			) {
+
+				this._sphericalEnd.radius = this._spherical.radius;
+				this._radiusVelocity.value = 0;
+
+			}
+
+			if (
+				( this._state & ACTION.ZOOM ) === ACTION.ZOOM ||
+				( this._state & ACTION.TOUCH_ZOOM ) === ACTION.TOUCH_ZOOM ||
+				( this._state & ACTION.TOUCH_ZOOM_TRUCK ) === ACTION.TOUCH_ZOOM_TRUCK ||
+				( this._state & ACTION.TOUCH_ZOOM_OFFSET ) === ACTION.TOUCH_ZOOM_OFFSET ||
+				( this._state & ACTION.TOUCH_ZOOM_ROTATE ) === ACTION.TOUCH_ZOOM_ROTATE
+			) {
+
+				this._zoomEnd = this._zoom;
+				this._zoomVelocity.value = 0;
+
+			}
+
+			if (
+				( this._state & ACTION.OFFSET ) === ACTION.OFFSET ||
+				( this._state & ACTION.TOUCH_OFFSET ) === ACTION.TOUCH_OFFSET ||
+				( this._state & ACTION.TOUCH_DOLLY_OFFSET ) === ACTION.TOUCH_DOLLY_OFFSET ||
+				( this._state & ACTION.TOUCH_ZOOM_OFFSET ) === ACTION.TOUCH_ZOOM_OFFSET
+			) {
+
+				this._focalOffsetEnd.copy( this._focalOffset );
+				this._focalOffsetVelocity.set( 0, 0, 0 );
+
+			}
+
 			this.dispatchEvent( { type: 'controlstart' } );
 
 		};
